f8dd90cb2ae0910db5619c6b714e928c1d56e92a
wiserkuo <wiserkuo@gmail.com>
2015-01-28 17:29:02 +0800

    initial , google map sdk , basic gmsmapview

---
+++ Feed Me/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  // 1
+  let googleMapsApiKey = "AIzaSyAEW1RRN2ptWGvXGvDZ-kkk_3FNoYgKBhI"
+  
+  func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+    // 2
+    GMSServices.provideAPIKey(googleMapsApiKey)
+    return true
+  }
+}
+
