09804fee0fb58011c7b1d9cea332ba60232cfb1f
wiserkuo <wiserkuo@wiserkuos-MacBook-Pro.local>
2015-02-04 00:55:19 +0800

    SegmentedControl,Getting Location,Geocoding
    Normal,Satellite,Hybrid mode via UISegmentedControl
    Request the permission of location via CLLocationManagerDelegate
    Address Geocoding via UILabel ,GMSGeocoder, GMSMapViewDelegate

--- Feed Me/Controllers/MapViewController.swift
+++ Feed Me/Controllers/MapViewController.swift
@@ -8,16 +8,98 @@
 
 import UIKit
 
-class MapViewController: UIViewController, TypesTableViewControllerDelegate {
+class MapViewController: UIViewController, TypesTableViewControllerDelegate , CLLocationManagerDelegate , GMSMapViewDelegate
+{
   
-  @IBOutlet weak var mapView: GMSMapView!
-  @IBOutlet weak var mapCenterPinImage: UIImageView!
-  @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
-  var searchedTypes = ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
-  
-  override func viewDidLoad() {
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var mapCenterPinImage: UIImageView!
+    @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
+    var searchedTypes = ["bakery", "bar", "cafe", "grocery_or_supermarket", "restaurant"]
+
+    let locationManager = CLLocationManager()
+    
+    func mapView(mapView: GMSMapView!, willMove gesture: Bool) {
+        addressLabel.lock()
+    }
+    
+    func reverseGeocodeCoordinate(coordinate: CLLocationCoordinate2D) {
+        
+        // 1
+        let geocoder = GMSGeocoder()
+        
+        // 2
+        geocoder.reverseGeocodeCoordinate(coordinate) { response , error in
+            //Add this line
+            self.addressLabel.unlock()
+            if let address = response?.firstResult() {
+                
+                // 3
+                let lines = address.lines as [String]
+                self.addressLabel.text = join("\n", lines)
+                // 1
+                let labelHeight = self.addressLabel.intrinsicContentSize().height
+                self.mapView.padding = UIEdgeInsets(top: self.topLayoutGuide.length, left: 0, bottom: labelHeight, right: 0)
+                // 4
+                UIView.animateWithDuration(0.25) {
+                    //2
+                    self.pinImageVerticalConstraint.constant = ((labelHeight - self.topLayoutGuide.length) * 0.5)
+                    self.view.layoutIfNeeded()
+                }
+            }
+        }
+    }
+    func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
+        reverseGeocodeCoordinate(position.target)
+    }
+    
+    // 1
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 2
+        if status == .AuthorizedWhenInUse {
+            
+            // 3
+            locationManager.startUpdatingLocation()
+            
+            //4
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    // 5
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        if let location = locations.first as? CLLocation {
+            
+            // 6
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            
+            // 7
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+    
+    @IBAction func mapTypeSegmentPressed(sender: AnyObject) {
+        let segmentedControl = sender as UISegmentedControl
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = kGMSTypeNormal
+        case 1:
+            mapView.mapType = kGMSTypeSatellite
+        case 2:
+            mapView.mapType = kGMSTypeHybrid
+        default:
+            mapView.mapType = mapView.mapType
+        }
+    }
+    
+   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
+    mapView.delegate = self
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
   }
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
